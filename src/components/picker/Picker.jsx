2bd4f68abcb06056e277843ae7516af870a3131e
amel.chabah <amel.chabah@gael.fr>
2023-10-20 16:22:28 +0200

    fix/main picker title

--- src/components/picker/Picker.jsx
+++ src/components/picker/Picker.jsx
@@ -6,10 +6,11 @@ const Picker = () => {
     const pickVisualizer = (index) => {
         Scene.changeVisualizer(index)
     }
+
     return (
         <>
         <div className={styles.picker}>
-            <h4>Pick a visualizer</h4>
+            <h4 className='grey'>Pick a visualizer</h4>
             <div onClick={() => pickVisualizer('cube')}>Cube</div>
             <div onClick={() => pickVisualizer('line')}>Line</div>
             <div onClick={() => pickVisualizer('sphere')}>Sphere</div>
