305185fb529fad36041070c6e033213f18e01b4f
amel.chabah <amel.chabah@gael.fr>
2023-10-20 16:41:18 +0200

    fix/main logo animation

--- src/webgl/objects/LogoIut.js
+++ src/webgl/objects/LogoIut.js
@@ -6,6 +6,8 @@ export default class LogoIut {
     constructor() {
         this.group = null;
         this.material = new THREE.MeshNormalMaterial();
+        this.circles = [];
+        
         Scene.gltfLoader.load('/logo-iut.glb', (gltf) => {
             console.log(gltf.scene);
             this.group = gltf.scene;
@@ -14,16 +16,29 @@ export default class LogoIut {
                     object.material = this.material;
                 }
             })
+
+
+            this.icosphere = this.group.getObjectByName('Icosphere');
+            this.circle1 = this.group.getObjectByName('BezierCircle');
+            this.circle2 = this.group.getObjectByName('BezierCircle001');
+            this.circle3 = this.group.getObjectByName('BezierCircle002');
+            this.circle4 = this.group.getObjectByName('BezierCircle003');
+
+            this.circles = [this.circle1, this.circle2, this.circle3, this.circle4];
+            
             this.group.rotation.x = Math.PI / 2;
         })
     }
 
     tick(deltaTime) {
-        this.group.rotation.x += 0.001 * deltaTime;
-        this.group.rotation.z += 0.001 * deltaTime;
+        const remapped = 1 + AudioController.dataArray[0] / 255;
+        this.icosphere.scale.set(remapped, remapped, remapped); 
 
-        const rescale = 1 + AudioController.dataArray[0] / 255;
-
-        this.group.scale.set(rescale, rescale, rescale)
+        for (let i =0; i < this.circles.length; i++) {
+            const circle = this.circles[i];
+            const scale = 1 + AudioController.dataArray[i] / 255;
+            circle.scale.set(scale, scale, scale);
+            
+        }
     }
 }
